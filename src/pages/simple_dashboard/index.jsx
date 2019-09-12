8e23b53f064613e1158e283ae56a9d3fe141f714
Michael Luo <mluo@splunk.com>
2019-09-11 20:29:50 -0700

    update prepare script

--- src/pages/simple_dashboard/index.jsx
+++ src/pages/simple_dashboard/index.jsx
@@ -5,6 +5,7 @@ import DashboardCore from '@splunk/dashboard-core';
 import definition from './definition';
 import preset from './preset';
 
+// use DashboardCore to render a simple dashboard
 layout(<DashboardCore width="100%" height="calc(100vh - 78px)" definition={definition} preset={preset} />, {
     pageTitle: 'Simple Grid Dashboard',
     hideFooter: true,
