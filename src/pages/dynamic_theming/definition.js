8d4793042c4bdc307057e9fd95386fc7b6fc04ae
Michael Luo <mluo@splunk.com>
2019-09-13 11:35:33 -0700

    update examples

--- src/pages/dynamic_theming/definition.js
+++ src/pages/dynamic_theming/definition.js
@@ -6,6 +6,24 @@ export default {
                 query: 'index=_internal | stats count',
             },
         },
+        error_count_search: {
+            type: 'ds.search',
+            options: {
+                query: 'index=_internal error | stats count',
+            },
+        },
+        info_count_search: {
+            type: 'ds.search',
+            options: {
+                query: 'index=_internal info | stats count',
+            },
+        },
+        warning_count_search: {
+            type: 'ds.search',
+            options: {
+                query: 'index=_internal warning| stats count',
+            },
+        },
         event_by_component_search: {
             type: 'ds.search',
             options: {
@@ -32,7 +50,34 @@ export default {
                 position: {
                     x: 1,
                     y: 1,
-                    w: 12,
+                    w: 3,
+                    h: 2,
+                },
+            },
+            {
+                item: 'sv_info',
+                position: {
+                    x: 4,
+                    y: 1,
+                    w: 3,
+                    h: 2,
+                },
+            },
+            {
+                item: 'sv_warning',
+                position: {
+                    x: 7,
+                    y: 1,
+                    w: 3,
+                    h: 2,
+                },
+            },
+            {
+                item: 'sv_error',
+                position: {
+                    x: 10,
+                    y: 1,
+                    w: 3,
                     h: 2,
                 },
             },
@@ -62,9 +107,41 @@ export default {
         sv_total_event: {
             title: '_internal event count',
             type: 'viz.singlevalue',
-            options: {},
+            options: {
+                backgroundColor: '#53a051',
+            },
             dataSources: {
                 primary: 'total_count_search',
+            },
+        },
+        sv_error: {
+            title: 'error count',
+            type: 'viz.singlevalue',
+            options: {
+                backgroundColor: '#dc4e41',
+            },
+            dataSources: {
+                primary: 'error_count_search',
+            },
+        },
+        sv_warning: {
+            title: 'warning count',
+            type: 'viz.singlevalue',
+            options: {
+                backgroundColor: '#f8be34',
+            },
+            dataSources: {
+                primary: 'warning_count_search',
+            },
+        },
+        sv_info: {
+            title: 'info count',
+            type: 'viz.singlevalue',
+            options: {
+                backgroundColor: '#294e70',
+            },
+            dataSources: {
+                primary: 'info_count_search',
             },
         },
         sv_event_by_component: {
