4da05139d44a8a1e9157a03e34172dd86bdcc6b6
Peter Peterson <ppeterson@splunk.com>
2019-10-17 15:58:08 -0700

    fix: donut does not display results

--- src/pages/buttercup/index.jsx
+++ src/pages/buttercup/index.jsx
@@ -6,6 +6,8 @@ import DashboardCore, { themes as dashboardCoreThemes } from '@splunk/dashboard-
 import EnterpriseViewOnlyPreset, {
     themes as presetThemes,
 } from '@splunk/dashboard-presets/EnterpriseViewOnlyPreset';
+import Radar from '../radar/Radar';
+import Donut from '../donut/Donut';
 import definition from './definition.json';
 
 const themeKey = 'enterpriseDark';
@@ -14,15 +16,20 @@ const theme = {
     ...dashboardCoreThemes[themeKey],
     ...reactUIThemes[themeKey],
 };
+
+const Preset = {
+    ...EnterpriseViewOnlyPreset,
+    visualizations: {
+        ...EnterpriseViewOnlyPreset.visualizations,
+        'viz.radar': Radar,
+        'viz.donut': Donut,
+    },
+};
+
 // use DashboardCore to render a simple dashboard
 layout(
     <ThemeProvider theme={theme}>
-        <DashboardCore
-            width="100%"
-            height="calc(100vh - 78px)"
-            definition={definition}
-            preset={EnterpriseViewOnlyPreset}
-        />
+        <DashboardCore width="100%" height="calc(100vh - 78px)" definition={definition} preset={Preset} />
     </ThemeProvider>,
     {
         pageTitle: 'Buttercup Games Dashboard',
