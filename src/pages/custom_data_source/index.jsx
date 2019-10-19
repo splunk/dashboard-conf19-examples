ae09faa308dfce627af00b214af6111e6e23e3bc
Yuxiang Kou <38112661+ykou-splunk@users.noreply.github.com>
2019-10-18 21:56:20 -0700

    built custom data source

--- src/pages/custom_data_source/index.jsx
+++ src/pages/custom_data_source/index.jsx
@@ -3,15 +3,19 @@ import layout from '@splunk/react-page';
 import DashboardCore from '@splunk/dashboard-core';
 import EnterpriseViewOnlyPreset from '@splunk/dashboard-presets/EnterpriseViewOnlyPreset';
 import definition from './definition.json';
+import TodoDataSource from './TodoDataSource';
+
+const customPreset = {
+    ...EnterpriseViewOnlyPreset,
+    dataSources: {
+        ...EnterpriseViewOnlyPreset.dataSources,
+        todo: TodoDataSource,
+    },
+};
 
 // use DashboardCore to render a simple dashboard
 layout(
-    <DashboardCore
-        width="100%"
-        height="calc(100vh - 78px)"
-        definition={definition}
-        preset={EnterpriseViewOnlyPreset}
-    />,
+    <DashboardCore width="100%" height="calc(100vh - 78px)" definition={definition} preset={customPreset} />,
     {
         pageTitle: 'Custom Data Source',
         hideFooter: true,
